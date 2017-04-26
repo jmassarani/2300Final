6fbac4c18a1705d257c328aeed6cf7a10663c23b
jmassarani <julianma95@gmail.com>
2017-04-26 15:19:25 -0400

    made basic php for contact form

--- contact.php
+++ contact.php
@@ -1,18 +1,37 @@
 <!doctype html>
-
 <html>
-	<head>
-		<title>Home</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="initial-scale=1.0">
-        <?php 
+
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <?php 
     require_once('includes/config.php');
     include 'includes/nav.php';
     include 'includes/header.php';
 ?>
-	</head>
+</head>
 
-	<body>
+<body class = "contact">
+    <form class = "contact_form" method = post>
+        <label>Name</label>
+        <input name="name" placeholder="Enter Your Name" required>*
+        <label>Email</label>
+        <input name="email" type="email" placeholder="Enter Your Email" required>*
+        
+        <label>Phone Number</label>
+        <input name="phone" type="text" placeholder="(Optional)">
+        <label>Message</label>
+        <textarea name="message" placeholder="Type Here" required></textarea>
+        <input id="submit" name="submit" type="submit" value="Submit"> </form>
+</body>
 
-	</body>
+    
+    <?php
+    $name = filter_input(INPUT_POST,'name');
+    $email = filter_input(INPUT_POST,'email');
+    $phone_number = filter_input(INPUT_POST,'phone');
+    $message = filter_input(INPUT_POST,'message');
+    
+    ?>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,9 @@
 <head>
         <link rel="stylesheet" type="text/css" href="styles/style.css">
         <meta charset="UTF-8">
-        <meta name="viewport" content="initial-scale=1.0"> </head>
+        <meta name="viewport" content="initial-scale=1.0">
+        <link href = "https://d1b10bmlvqabco.cloudfront.net/attach/iy7lf5qn2hw6nz/hz6yrg3d32t1gb/j1rydk69e6fa/bootstrap.min.css" rel = "stylesheet">
+    </head>
 
 <body> </body>
 
