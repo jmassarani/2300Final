3596839613cfec5bc19e84c65b39bbfd85bf5d2f
Wendy Huang <Wendy@dhcp-gs-6033.eduroam.cornell.edu>
2017-05-01 14:58:37 -0400

    * basic layout for home page * added header to home page * renamed css file to fit what is actually in it

--- about.php
+++ about.php
@@ -7,12 +7,18 @@
 		<meta name="viewport" content="initial-scale=1.0">
         <?php 
     require_once('includes/config.php');
-    include 'includes/nav.php';
     include 'includes/header.php';
 ?>
 	</head>
 
 	<body>
-
+        <header>
+            <h1>Queen of Schmooze</h1>
+        </header>
+        <?php
+            global $current_page;
+            $current_page = "Home";
+            include "includes/nav.php"
+        ?>
 	</body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="stylesheet" type="text/css" href="styles/nav.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="initial-scale=1.0">
         <link href = "https://d1b10bmlvqabco.cloudfront.net/attach/iy7lf5qn2hw6nz/hz6yrg3d32t1gb/j1rydk69e6fa/bootstrap.min.css" rel = "stylesheet">
