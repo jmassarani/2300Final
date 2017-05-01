bf7432a9d157f7b5512f7aec3c016906fb38d627
Wendy Huang <Wendy@dhcp-gs-6033.eduroam.cornell.edu>
2017-05-01 15:20:11 -0400

    * made changes to other pages to reflect the change in made in nav.php

--- contact.php
+++ contact.php
@@ -1,30 +1,37 @@
 <!doctype html>
 <html>
 
-<head>
-    <title>Home</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1.0">
-    <?php 
-    require_once('includes/config.php');
-    include 'includes/nav.php';
-    include 'includes/header.php';
-?>
-</head>
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="initial-scale=1.0">
+        <?php 
+        require_once('includes/config.php');
+        include 'includes/header.php';
+    ?>
+    </head>
 
-<body class = "contact">
-    <form class = "contact_form" method = post>
-        <label>Name</label>
-        <input name="name" placeholder="Enter Your Name" required>*
-        <label>Email</label>
-        <input name="email" type="email" placeholder="Enter Your Email" required>*
-        
-        <label>Phone Number</label>
-        <input name="phone" type="text" placeholder="(Optional)">
-        <label>Message</label>
-        <textarea name="message" placeholder="Type Here" required></textarea>
-        <input id="submit" name="submit" type="submit" value="Submit"> </form>
-</body>
+    <body class = "contact">
+        <header>
+            <h1>Queen of Schmooze</h1>
+        </header>
+        <?php
+            global $current_page;
+            $current_page = "Contaact";
+            include "includes/nav.php"
+        ?>
+        <form class = "contact_form" method = post>
+            <label>Name</label>
+            <input name="name" placeholder="Enter Your Name" required>*
+            <label>Email</label>
+            <input name="email" type="email" placeholder="Enter Your Email" required>*
+
+            <label>Phone Number</label>
+            <input name="phone" type="text" placeholder="(Optional)">
+            <label>Message</label>
+            <textarea name="message" placeholder="Type Here" required></textarea>
+            <input id="submit" name="submit" type="submit" value="Submit"> </form>
+    </body>
 
     
     <?php
