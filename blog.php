3fe6f44006add7a9c25a7289e2cb576163b8ed02
Wendy Huang <Wendy@Wendys-Air.T-mobile.com>
2017-05-04 00:44:58 -0400

    * styling edits to the navbar * added sidebar to blog page

--- blog.php
+++ blog.php
@@ -23,30 +23,41 @@
             $current_page = "Blog";
             include "includes/nav.php"
         ?>
-    <div id="wrapper" class="container">
+        <div id="wrapper" class="container">
+            <div class="col-sm-3">
+                <h1>Blog Posts</h1>
+                <ul>
+                    <?php
+                        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+                        $sql = $mysqli->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                        while($row = $sql->fetch_assoc()){
+                            echo '<li><a href=#'.$row['postID'].'>'.$row['postTitle'].'</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+            <div class="col-sm-9">
+                <?php
+                    try {
+                        $sql = $mysqli->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+                        while($row = $sql->fetch_assoc()){
+                            echo '<div>';
+                            
+                            echo '<h1 id='.$row['postID'].'><a href="showpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+                            
+                            echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+                            
+                            echo '<p>'.$row['postDesc'].'</p>';				
+                            echo '<p><a href="showpost.php?id='.$row['postID'].'">Read More</a></p>';		
+                            echo '</div>';
+                        }
+                    } catch(PDOException $e) {
+                        echo $e->getMessage();
+                    }
+                ?>
+            </div>
 
-		<h1>Blog</h1>
-
-		<?php
-                $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-			try {
-				$sql = $mysqli->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
-				while($row = $sql->fetch_assoc()){
-					
-					echo '<div>';
-						echo '<h1><a href="showpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-						echo '<p>'.$row['postDesc'].'</p>';				
-						echo '<p><a href="showpost.php?id='.$row['postID'].'">Read More</a></p>';				
-					echo '</div>';
-				}
-			} catch(PDOException $e) {
-			    echo $e->getMessage();
-			}
-		?>
-
-	</div>
-        
+        </div>
         
 	</body>
 </html>
--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,18 @@
-
 window.onload = function () {
-  
-      $(window).scroll(function () {
+$(document).ready(function(){
+//    window.scroll = function () {
+    $(window).scroll(function () {
           //if you hard code, then use console
           //.log to determine when you want the 
           //nav bar to stick.  
-          console.log($(window).scrollTop())
-        if ($(window).scrollTop() > 1072) {
+          console.log($(window).scrollTop());
+        if ($(window).scrollTop() > 64) {
           $('#nav_bar').addClass('navbar-fixed');
         }
-        if ($(window).scrollTop() < 1072) {
+        if ($(window).scrollTop() < 64) {
           $('#nav_bar').removeClass('navbar-fixed');
         }
       });
+})
+//    }
 }
