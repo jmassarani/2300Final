60182396494ece10b275770b6e8d0cec38ab64d6
Wendy Huang <Wendy@Wendys-Air.T-mobile.com>
2017-05-04 18:37:27 -0400

    * navbar fixes

--- includes/nav.php
+++ includes/nav.php
@@ -22,29 +22,23 @@
             foreach($nav_items as $title=>$link) {
                 printLink ($title, $link);
             }
-            print ("<li class='dropdown");
-            if ($GLOBALS["current_page"] == 'What is Schmooze') {
-                echo " active";
-            } 
-            print ("'>More</a><div class='dropdown-content'><a href='more.php'>What is Schmooze</a></div></li>");
-            if (isset($_SESSION['logged_user_by_sql'])) {
-                echo "<li><a class='logout_button";
-                if ($GLOBALS["current_page"] == 'Logout') {
-                    echo " active";
-                }
-                echo "' href='logout.php'>Logout</a></li>";
-            } else {
-                echo "<li><a class='login_button";
-                if ($GLOBALS["current_page"] == 'Login') {
-                    echo " active";
-                }
-                echo "' href='login.php'>Login</a></li>";
-            }
-        ?>
-    </ul>
-</nav>
-
-on";
+            // Blog: Show add new post page if admin login
+//            if (isset($_SESSION['logged_user_by_sql'])) {
+//                $stmt = $mysqli->stmt_init();
+//                $stmt->prepare("SELECT * FROM users WHERE username=?"); $stmt->bind_param('s', $post_username);
+//                $stmt->execute();
+//                $result=$stmt->get_result();
+//
+//                if ($result && $result->num_rows == 1) {
+//                    $row = $result->fetch_assoc();
+//                    $db_hash_password = $row['hashpassword'];
+//
+//                    if( password_verify( $post_password, $db_hash_password ) ) {
+//                        $db_username = $row['username'];
+//                        $_SESSION['logged_user_by_sql'] = $db_username;
+//
+//                    }
+////                echo "<li><a class='logout_button";
 ////                if ($GLOBALS["current_page"] == 'Logout') {
 ////                    echo " active";
 ////                }
