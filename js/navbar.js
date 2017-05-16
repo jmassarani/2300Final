70f07f6ed2cae042bd638255904458b1ad9e15d0
Wendy Huang <Wendy@dhcp-hol-2564.eduroam.cornell.edu>
2017-05-16 15:04:02 -0400

    deleted comments

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,4 @@
-//window.onload = function () {
-//    $(document).ready(function(){
-//        $(window).bind('scroll', function () {
-////            console.log($(window).scrollTop());
-//            if ($(window).scrollTop() > 63) {
-//              $('#nav_bar').addClass('navbar-fixed');
-//            }
-//            if ($(window).scrollTop() < 64) {
-//              $('#nav_bar').removeClass('navbar-fixed');
-//            } 
-// 
-//        });
-//    })
-//}
 window.onload = function () {
-//    $( ".cross" ).hide();
-//    $( ".menu" ).hide();
     function checkWidth(init)
     {
         /*If browser resized, check width again */
