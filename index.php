56c73fb2587e3ad636218a92671408d8bbe9ece2
Wendy Huang <Wendy@dhcp-hol-2564.eduroam.cornell.edu>
2017-05-16 13:09:58 -0400

    edit home page

--- index.php
+++ index.php
@@ -25,6 +25,8 @@
                 <h2>You're a great organization with great ideas. Do you want to show it? Why leave things to chance? Sometimes an extra push is needed!</h2>
                 <p>One of our biggest challenges is how to connect with one another in a genuine way.  We are here to help you to make this happen. </p>
             </div> 
+            <img class="col-sm-12" src="images/networking2.jpg" alt="homepage">
+       
         </div>
         <footer>
         </footer>
