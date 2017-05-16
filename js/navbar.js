c872e07352444f42a1f83091746ce29b0a7ebf5e
Wendy Huang <Wendy@dhcp-hol-2564.eduroam.cornell.edu>
2017-05-16 16:00:16 -0400

    navbar breakpoint changed

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ window.onload = function () {
     function checkWidth(init)
     {
         /*If browser resized, check width again */
-        if ($(window).width() <= 920) {
+        if ($(window).width() <= 930) {
             
             if (!init) {
                 $('.menu').css("display", "none");
